2c7f0a7d8650ebcba929d886766fefc6861c3cff
william dutton <will.dutt@gmail.com>
2020-07-06 15:45:39 +1000

    make travis great again

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,16 +5,34 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
+sudo apt-get install postgresql-$PGVERSION solr-jetty openjdk-8-jdk libcommons-fileupload-java:amd64
+echo "Start postgres"
+sudo service postgresql start
+
 
 echo "Installing CKAN and its Python dependencies..."
-git clone https://github.com/ckan/ckan
+git clone https://github.com/$CKAN_GIT_REPO
 cd ckan
 git checkout $CKAN_BRANCH
 python setup.py develop
-pip install -r requirements.txt --allow-all-external
-pip install -r dev-requirements.txt --allow-all-external
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
 cd -
+
+echo "start solr"
+# Fix solr-jetty starting issues https://stackoverflow.com/a/56007895
+# https://github.com/Zharktas/ckanext-report/blob/py3/bin/travis-run.bash
+sudo mkdir /etc/systemd/system/jetty9.service.d
+printf "[Service]\nReadWritePaths=/var/lib/solr" | sudo tee /etc/systemd/system/jetty9.service.d/solr.conf
+sed '16,21d' /etc/solr/solr-jetty.xml | sudo tee /etc/solr/solr-jetty.xml
+sudo systemctl daemon-reload
+
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_ARGS=\"jetty.http.port=8983\"\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty9
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty9 restart
+
+# Wait for jetty9 to start
+timeout 20 bash -c 'while [[ "$(curl -s -o /dev/null -I -w %{http_code} http://localhost:8983)" != "200" ]]; do sleep 2;done'
 
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
@@ -35,3 +53,4 @@ mkdir subdir
 mv test.ini subdir
 
 echo "travis-build.bash is done."
+
