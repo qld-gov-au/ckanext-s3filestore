5d247153cc89bd3e9da20994bed88bfc4f38375b
ThrawnCA <shell_layer-github@yahoo.com.au>
2020-07-09 16:23:14 +1000

    make build more robust
    - don't fail if directories already exist
    - install more packages we know we'll need

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,7 +5,7 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty openjdk-8-jdk libcommons-fileupload-java:amd64 redis-server
+sudo apt-get install systemd git python-pip postgresql-$PGVERSION solr-jetty openjdk-8-jdk libcommons-fileupload-java:amd64 redis-server
 echo "Start postgres"
 sudo service postgresql start
 
@@ -18,7 +18,9 @@ pip install "moto[server]"
 moto_server s3 &
 
 echo "Installing CKAN and its Python dependencies..."
-git clone https://github.com/$CKAN_GIT_REPO
+if [ ! -d ckan ]; then
+  git clone https://github.com/$CKAN_GIT_REPO
+fi
 cd ckan
 git checkout $CKAN_BRANCH
 python setup.py develop
@@ -29,7 +31,7 @@ cd -
 echo "start solr"
 # Fix solr-jetty starting issues https://stackoverflow.com/a/56007895
 # https://github.com/Zharktas/ckanext-report/blob/py3/bin/travis-run.bash
-sudo mkdir /etc/systemd/system/jetty9.service.d
+sudo mkdir -p /etc/systemd/system/jetty9.service.d
 printf "[Service]\nReadWritePaths=/var/lib/solr" | sudo tee /etc/systemd/system/jetty9.service.d/solr.conf
 sed '16,21d' /etc/solr/solr-jetty.xml | sudo tee /etc/solr/solr-jetty.xml
 sudo systemctl daemon-reload
@@ -56,8 +58,8 @@ pip install -r requirements.txt
 pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
-mkdir subdir
-mv test.ini subdir
+mkdir -p subdir
+cp test.ini subdir
 
 
 
