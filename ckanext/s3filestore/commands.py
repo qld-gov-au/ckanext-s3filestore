e2bd65e65a914e4d5acdd0cca2777b8129cc72af
duskobogdanovski <dule.bogdanovski@gmail.com>
2020-11-30 11:57:11 +0100

    Support for using AMI roles instead of hardcoding an access key.

--- ckanext/s3filestore/commands.py
+++ ckanext/s3filestore/commands.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
+
 import sys
-import boto
+
 from ckantoolkit import config
 import ckantoolkit as toolkit
+import ckanext.s3filestore.uploader
 
 
 class TestConnection(toolkit.CkanCommand):
@@ -28,32 +30,25 @@ class TestConnection(toolkit.CkanCommand):
 
     def check_config(self):
         exit = False
-        for key in ('ckanext.s3filestore.aws_access_key_id',
-                    'ckanext.s3filestore.aws_secret_access_key',
-                    'ckanext.s3filestore.aws_bucket_name'):
+        required_keys = ('ckanext.s3filestore.aws_bucket_name',)
+        if not config.get('ckanext.s3filestore.aws_use_ami_role'):
+            required_keys += ('ckanext.s3filestore.aws_access_key_id',
+                              'ckanext.s3filestore.aws_secret_access_key')
+        for key in required_keys:
             if not config.get(key):
-                print('You must set the "{0}" option in your ini file'.format(
-                    key))
+                print('You must set the "{0}" option in your ini file'.format(key))
                 exit = True
         if exit:
             sys.exit(1)
 
         print('All configuration options defined')
         bucket_name = config.get('ckanext.s3filestore.aws_bucket_name')
-        public_key = config.get('ckanext.s3filestore.aws_access_key_id')
-        secret_key = config.get('ckanext.s3filestore.aws_secret_access_key')
 
-        S3_conn = boto.connect_s3(public_key, secret_key)
+        try:
+            ckanext.s3filestore.uploader.BaseS3Uploader().get_s3_bucket(bucket_name)
+        except ckanext.S3FileStoreException as ex:
+            print('An error was found while finding or creating the bucket:')
+            print(str(ex))
+            sys.exit(1)
 
-        # Check if bucket exists
-        bucket = S3_conn.lookup(bucket_name)
-        if bucket is None:
-            print('Bucket {0} does not exist, trying to create it...'.format(
-                bucket_name))
-            try:
-                bucket = S3_conn.create_bucket(bucket_name)
-            except boto.exception.StandardError as e:
-                print('An error was found while creating the bucket:')
-                print(str(e))
-                sys.exit(1)
         print('Configuration OK!')
