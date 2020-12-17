6de1d8358e52971aff449967df663f160c8bb77c
ThrawnCA <shell_layer-github@yahoo.com.au>
2020-12-17 11:25:17 +1000

    [QOL-6421] put file migration logic into a new paster command
    - TODO add ability to migrate individual resources, delete file after uploading

--- ckanext/s3filestore/commands.py
+++ ckanext/s3filestore/commands.py
@@ -1,9 +1,13 @@
 from __future__ import print_function
 
+import boto3
+import os
 import sys
 
-from ckantoolkit import config
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 import ckantoolkit as toolkit
+from ckantoolkit import config
 import ckanext.s3filestore.uploader
 
 
@@ -12,9 +16,13 @@ class TestConnection(toolkit.CkanCommand):
 
     Usage:
 
-        paster s3 check-config
+        s3 check-config
 
             Checks if the configuration entered in the ini file is correct
+
+        s3 upload [all]
+
+            Uploads existing files from disk to S3.
 
     '''
     summary = __doc__.split('\n')[0]
@@ -22,15 +30,23 @@ class TestConnection(toolkit.CkanCommand):
     min_args = 1
 
     def command(self):
-        self._load_config()
         if not self.args:
             print(self.usage)
-        elif self.args[0] == 'check-config':
+            sys.exit(1)
+        self._load_config()
+        if self.args[0] == 'check-config':
             self.check_config()
+        elif self.args[0] == 'upload':
+            if len(self.args) < 2 or args[1] == 'all':
+                self.upload_all()
+        else:
+            self.parser.error('Unrecognized command')
 
     def check_config(self):
         exit = False
-        required_keys = ('ckanext.s3filestore.aws_bucket_name',)
+        required_keys = ('ckanext.s3filestore.aws_bucket_name',
+                         'ckanext.s3filestore.region_name',
+                         'ckanext.s3filestore.signature_version')
         if not config.get('ckanext.s3filestore.aws_use_ami_role'):
             required_keys += ('ckanext.s3filestore.aws_access_key_id',
                               'ckanext.s3filestore.aws_secret_access_key')
@@ -52,3 +68,63 @@ class TestConnection(toolkit.CkanCommand):
             sys.exit(1)
 
         print('Configuration OK!')
+
+    def upload_all(self):
+        BASE_PATH = config.get('ckan.storage_path', '/var/lib/ckan/default/resources')
+        SQLALCHEMY_URL = config.get('sqlalchemy.url', 'postgresql://user:pass@localhost/db')
+        if config.get('ckanext.s3filestore.aws_use_ami_role', False):
+            AWS_ACCESS_KEY_ID = None
+            AWS_SECRET_ACCESS_KEY = None
+        else:
+            AWS_ACCESS_KEY_ID = config.get('ckanext.s3filestore.aws_access_key_id')
+            AWS_SECRET_ACCESS_KEY = config.get('ckanext.s3filestore.aws_secret_access_key')
+        AWS_BUCKET_NAME = config.get('ckanext.s3filestore.aws_bucket_name')
+        AWS_S3_ACL = config.get('ckanext.s3filestore.acl', 'public-read')
+        resource_ids_and_paths = {}
+
+        for root, dirs, files in os.walk(BASE_PATH):
+            if files:
+                resource_id = root.split('/')[-2] + root.split('/')[-1] + files[0]
+                resource_ids_and_paths[resource_id] = os.path.join(root, files[0])
+
+        print('Found {0} resource files in the file system'.format(
+            len(resource_ids_and_paths.keys())))
+
+        engine = create_engine(SQLALCHEMY_URL)
+        connection = engine.connect()
+
+        resource_ids_and_names = {}
+
+        try:
+            for resource_id, file_path in resource_ids_and_paths.iteritems():
+                resource = connection.execute(text('''
+                    SELECT id, url, url_type
+                    FROM resource
+                    WHERE id = :id
+                '''), id=resource_id)
+                if resource.rowcount:
+                    _id, url, _type = resource.first()
+                    if _type == 'upload' and url:
+                        file_name = url.split('/')[-1] if '/' in url else url
+                        resource_ids_and_names[_id] = file_name.lower()
+        finally:
+            connection.close()
+            engine.dispose()
+
+        print('{0} resources matched on the database'.format(
+            len(resource_ids_and_names.keys())))
+
+        # todo: move to plugin initi so we don't need to reinit secrets
+        s3_connection = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        bucket = s3_connection.Bucket(AWS_BUCKET_NAME)
+
+        uploaded_resources = []
+        for resource_id, file_name in resource_ids_and_names.iteritems():
+            key = 'resources/{resource_id}/{file_name}'.format(
+                resource_id=resource_id, file_name=file_name)
+
+            s3_connection.Object(AWS_BUCKET_NAME, key).put(Body=open(resource_ids_and_paths[resource_id]), ACL=AWS_S3_ACL)
+            uploaded_resources.append(resource_id)
+            print('Uploaded resource {0} ({1}) to S3'.format(resource_id, file_name))
+
+        print('Done, uploaded {0} resources to S3'.format(len(uploaded_resources)))
