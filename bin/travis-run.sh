62dcfe80c7774d17089358f5018f6a487cef102c
ThrawnCA <shell_layer-github@yahoo.com.au>
2020-12-23 07:55:14 +1000

    [QOL-7545] revert Keitaro tests for now to fix build

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-pytest --ckan-ini=subdir/test.ini --cov=ckanext.s3filestore --disable-warnings ckanext/s3filestore/tests
+nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.s3filestore --cover-inclusive --cover-erase --cover-tests
