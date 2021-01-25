d54348639f898c17ca2fc77e23c39eb0b8039b8f
william dutton <will.dutt@gmail.com>
2021-01-25 10:48:26 +1000

    use docker compose way without local ckan install

---
+++ .docker/scripts/serve.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e
+
+dockerize -wait tcp://postgres:5432 -timeout 1m
+dockerize -wait tcp://solr:8983 -timeout 1m
+
+sed -i "s@SITE_URL@${SITE_URL}@g" /app/ckan/default/production.ini
+
+python -m smtpd -n -c DebuggingServer localhost:25 &
+
+. /app/ckan/default/bin/activate \
+    && paster serve /app/ckan/default/production.ini
