2b6ef3591f912edc297f9b3b0cee477f14a452a4
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-11-17 09:17:52 +1000

    [DC-27824] add initial plugin test

---
+++ ckanext/s3filestore/tests/test_plugin.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+import mock
+
+from ckanext.s3filestore.plugin import S3FileStorePlugin
+
+
+class TestS3Plugin():
+
+    def test_update_config(self):
+        '''Plugin sets template directories'''
+        plugin = S3FileStorePlugin()
+        config = {}
+        with mock.patch('ckanext.s3filestore.plugin.toolkit') as mock_toolkit:
+            plugin.update_config(config)
+            mock_toolkit.add_template_directory.assert_called_once_with(config, 'templates')
+            mock_toolkit.add_template_directory.assert_called_once_with(config, 'theme/templates')
