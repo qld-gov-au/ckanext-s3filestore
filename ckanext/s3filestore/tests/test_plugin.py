339c5fe5fe81d6c664e40ac99f964692f5597c5f
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-11-17 10:07:48 +1000

    [DC-27824] add test for updating object visibility

--- ckanext/s3filestore/tests/test_plugin.py
+++ ckanext/s3filestore/tests/test_plugin.py
@@ -7,13 +7,26 @@ from ckanext.s3filestore.plugin import S3FileStorePlugin
 
 class TestS3Plugin():
 
+    def setup(self):
+        self.plugin = S3FileStorePlugin()
+
     def test_update_config(self):
         '''Plugin sets template directories'''
-        plugin = S3FileStorePlugin()
         config = {}
         with mock.patch('ckanext.s3filestore.plugin.toolkit') as mock_toolkit:
-            plugin.update_config(config)
+            self.plugin.update_config(config)
             mock_toolkit.add_template_directory.assert_has_calls(
                 [mock.call(config, 'templates'),
                  mock.call(config, 'theme/templates')]
             )
+
+    def test_package_after_update(self):
+        '''S3 object visibility is updated to match package'''
+        pkg_dict = {'id': 'test-package',
+                    'resources': [{'id': 'test-resource'}]}
+        with mock.patch('ckanext.s3filestore.plugin.toolkit') as mock_toolkit:
+            with mock.patch('ckanext.s3filestore.plugin.s3_uploader.S3ResourceUploader') as mock_uploader:
+                mock_toolkit.get_action.return_value = lambda: pkg_dict
+                self.plugin.after_update({}, pkg_dict)
+                mock_uploader.update_visibility.assert_called_once_with(
+                    'test-resource', 'public-read')
