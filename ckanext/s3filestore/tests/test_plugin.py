6068b01b4d5daadb0d138c519b4f155f7976ebcf
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-02-08 15:54:50 +1000

    [QOL-8612] add unit test for enqueue syntax

--- ckanext/s3filestore/tests/test_plugin.py
+++ ckanext/s3filestore/tests/test_plugin.py
@@ -3,6 +3,7 @@
 import mock
 from parameterized import parameterized
 
+from ckanext.s3filestore import tasks
 from ckanext.s3filestore.plugin import S3FileStorePlugin
 
 
@@ -27,7 +28,7 @@ class TestS3Plugin():
         (False, 'public-read')
     ])
     def test_package_after_update(self, is_private, expected_acl):
-        '''S3 object visibility is updated to match package'''
+        ''' S3 object visibility is updated to match package'''
         pkg_dict = {'id': 'test-package',
                     'resources': [{'id': 'test-resource'}]}
         if is_private is not None:
@@ -42,3 +43,19 @@ class TestS3Plugin():
                 self.plugin.after_update({}, pkg_dict)
                 mock_uploader.update_visibility.assert_called_once_with(
                     'test-resource', target_acl=expected_acl)
+
+    def test_enqueueing_visibility_update(self):
+        ''' Asynchronous job is created to update object visibility.
+        '''
+        # ensure that we don't trigger errors
+        self.plugin.enqueue_resource_visibility_update_job('private', 'abcde')
+
+        # check that the args were actually passed in
+        with mock.patch('rq.Queue.enqueue_call') as enqueue_call:
+            self.plugin.enqueue_resource_visibility_update_job('private', 'abcde')
+            enqueue_call.assert_called_once_with(
+                func=tasks.s3_afterUpdatePackage,
+                args=[],
+                kwargs={'visibility_level': 'private', 'pkg_id': 'abcde'},
+                ttl=86400
+            )
